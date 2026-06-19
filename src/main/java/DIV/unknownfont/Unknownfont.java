74dcf4397f72e76847655b313589638c757af5bf
DIV <k7611066@gmail.com>
2026-06-19 09:35:19 +0900

    Initial commit: clean Forge 1.20.1 MDK skeleton for UnknownFont

---
+++ src/main/java/DIV/unknownfont/Unknownfont.java
@@ -0,0 +1,27 @@
+package DIV.unknownfont;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(Unknownfont.MODID)
+public class Unknownfont {
+
+    public static final String MODID = "unknownfont";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Unknownfont() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+}
